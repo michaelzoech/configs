0395757e3556233942b03f475bba3f2642b31332
Michael Zoech <michi.zoech@gmail.com>
2010-04-13 23:51:14 +0200

    rename modMask to modm

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -44,13 +44,13 @@ myTerminal      = "terminal"
 --
 myBorderWidth   = 2
 
--- modMask lets you specify which modkey you want to use. The default
+-- modm lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
 -- ("right alt"), which does not conflict with emacs keybindings. The
 -- "windows key" is usually mod4Mask.
 --
-myModMask       = mod4Mask
-modMask2        = mod5Mask
+modm       = mod4Mask
+modm2        = mod5Mask
 
 -- The mask for the numlock key. Numlock status is "masked" from the
 -- current modifier status, so the keybindings will work with numlock on or
@@ -89,95 +89,95 @@ myFocusedBorderColor = "#cd8b00"
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 
     -- launch a terminal
-    [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modMask,               xK_r     ), shellPrompt myXPConfig)
+    , ((modm,               xK_r     ), shellPrompt myXPConfig)
 
     -- launch gmrun
-    --, ((modMask .|. shiftMask, xK_r     ), spawn "gmrun")
+    --, ((modm .|. shiftMask, xK_r     ), spawn "gmrun")
 
     -- close focused window
-    , ((modMask .|. shiftMask, xK_c     ), kill)
+    , ((modm .|. shiftMask, xK_c     ), kill)
 
      -- Rotate through the available layout algorithms
-    , ((modMask,               xK_space ), sendMessage NextLayout)
+    , ((modm,               xK_space ), sendMessage NextLayout)
 
     --  Reset the layouts on the current workspace to default
-    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
 
     -- Resize viewed windows to the correct size
-    --, ((modMask,               xK_n     ), refresh)
+    --, ((modm,               xK_n     ), refresh)
 
     -- Move focus to the next window
-    , ((modMask,               xK_Tab   ), windows W.focusDown)
-    , ((modMask2,              xK_Tab   ), windows W.focusDown)
+    , ((mod1Mask,           xK_Tab   ), windows W.focusDown)
+    , ((modm2,              xK_Tab   ), windows W.focusDown)
 
     -- Move focus to the next window
-    , ((modMask,               xK_j     ), windows W.focusDown)
+    , ((modm,               xK_j     ), windows W.focusDown)
 
     -- Move focus to the previous window
-    , ((modMask,               xK_k     ), windows W.focusUp  )
+    , ((modm,               xK_k     ), windows W.focusUp  )
 
     -- Move focus to the master window
-    , ((modMask,               xK_m     ), windows W.focusMaster  )
+    , ((modm,               xK_m     ), windows W.focusMaster  )
 
     -- Swap the focused window and the master window
-    , ((modMask,               xK_Return), windows W.swapMaster)
+    , ((modm,               xK_Return), windows W.swapMaster)
 
     -- Swap the focused window with the next window
-    , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown  )
+    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
 
     -- Swap the focused window with the previous window
-    , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp    )
+    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
 
     -- Shrink the master area
-    , ((modMask,               xK_h     ), sendMessage Shrink)
+    , ((modm,               xK_h     ), sendMessage Shrink)
 
     -- Expand the master area
-    , ((modMask,               xK_l     ), sendMessage Expand)
+    , ((modm,               xK_l     ), sendMessage Expand)
 
     -- Push window back into tiling
-    , ((modMask,               xK_t     ), withFocused $ windows . W.sink)
+    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
 
     -- Increment the number of windows in the master area
-    , ((modMask              , xK_w     ), sendMessage (IncMasterN 1))
+    , ((modm              , xK_w     ), sendMessage (IncMasterN 1))
 
     -- Deincrement the number of windows in the master area
-    , ((modMask              , xK_v     ), sendMessage (IncMasterN (-1)))
+    , ((modm              , xK_v     ), sendMessage (IncMasterN (-1)))
 
     -- Quit xmonad
-    , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
 
     -- Restart xmonad
-    , ((modMask              , xK_q     ), restart "xmonad" True)
+    , ((modm              , xK_q     ), restart "xmonad" True)
 
     -- goto workspace and focus window
-    , ((modMask .|. shiftMask, xK_g     ), gotoMenu)
+    , ((modm .|. shiftMask, xK_g     ), gotoMenu)
 
     -- bring and focus window
-    , ((modMask .|. shiftMask, xK_b     ), bringMenu)
+    , ((modm .|. shiftMask, xK_b     ), bringMenu)
 
-    , ((modMask              , xK_s     ), scratchpadSpawnActionTerminal "urxvt")
+    , ((modm              , xK_s     ), scratchpadSpawnActionTerminal "urxvt")
 
-    , ((modMask              , xK_n     ), toggleWS)
+    , ((modm              , xK_n     ), toggleWS)
 
-    , ((modMask              , xK_slash ), spawn "aumix -v -7")
-    , ((modMask              , xK_equal ), spawn "aumix -v +7")
+    , ((modm              , xK_slash ), spawn "aumix -v -7")
+    , ((modm              , xK_equal ), spawn "aumix -v +7")
 
-    , ((0                    , 0x1008FF11), spawn "aumix -v -5")
-    , ((0                    , 0x1008FF13), spawn "aumix -v +5")
-    , ((0                    , 0x1008FF12), spawn "aumix -v 0")
+    , ((0                 , 0x1008FF11), spawn "aumix -v -5")
+    , ((0                 , 0x1008FF13), spawn "aumix -v +5")
+    , ((0                 , 0x1008FF12), spawn "aumix -v 0")
 
-    , ((0                    , 0x1008FF02), spawn "sudo /usr/sbin/brightness up")
-    , ((0                    , 0x1008FF03), spawn "sudo /usr/sbin/brightness down")
+    , ((0                 , 0x1008FF02), spawn "sudo /usr/sbin/brightness up")
+    , ((0                 , 0x1008FF03), spawn "sudo /usr/sbin/brightness down")
 
-    , ((modMask2             , xK_apostrophe), spawn "mpc prev")
-    , ((modMask2             , xK_comma ), spawn "mpc next")
-    , ((modMask2             , xK_period), spawn "mpc toggle")
-    , ((modMask2             , xK_p     ), spawn "mpc seek -2%")
-    , ((modMask2             , xK_y     ), spawn "mpc seek +2%")
-    , ((modMask2             , xK_a     ), floatNext True >> spawn "Terminal --geometry=80x45+420+50 -e ncmpc")
+    , ((modm2             , xK_apostrophe), spawn "mpc prev")
+    , ((modm2             , xK_comma ), spawn "mpc next")
+    , ((modm2             , xK_period), spawn "mpc toggle")
+    , ((modm2             , xK_p     ), spawn "mpc seek -2%")
+    , ((modm2             , xK_y     ), spawn "mpc seek +2%")
+    , ((modm2             , xK_a     ), floatNext True >> spawn "Terminal --geometry=80x45+420+50 -e ncmpc")
     ]
     ++
 
@@ -185,7 +185,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
     --
-    [((m .|. modMask2, k), windows $ f i)
+    [((m .|. modm2, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     -- ++
@@ -194,7 +194,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     --
-    --[((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    --[((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
     --    | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
     --    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
@@ -204,13 +204,13 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
 
     -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
+    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w))
 
     -- mod-button2, Raise the window to the top of the stack
-    , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
+    , ((modm, button2), (\w -> focus w >> windows W.swapMaster))
 
     -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))
+    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w))
 
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
@@ -326,7 +326,7 @@ main =
         terminal           = myTerminal,
         focusFollowsMouse  = myFocusFollowsMouse,
         borderWidth        = myBorderWidth,
-        modMask            = myModMask,
+        modMask            = modm,
         numlockMask        = myNumlockMask,
         workspaces         = myWorkspaces,
         normalBorderColor  = myNormalBorderColor,
