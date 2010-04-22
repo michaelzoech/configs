a7d62c8baca852241e6340200d17d1aaf740a8a7
Michael Zoech <michi.zoech@gmail.com>
2010-04-22 02:07:18 +0200

    enable keys for two physical monitors

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -202,15 +202,15 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
         | mask <- [mod1Mask, modm2]
         , (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-    -- ++
+    ++
 
     --
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     --
-    --[((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-    --    | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-    --    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+    [((m .|. modm2, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_period, xK_comma] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
