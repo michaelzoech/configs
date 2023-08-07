c57091a28885de87d786c35a501b2b1b3fe11cb7
Michael Zoech <michi.zoech@gmail.com>
2023-08-06 21:13:34 -0700

    Jumping with { and } should center vertically similar to C-d and C-u

--- config/nvim/lua/maik/remap.lua
+++ config/nvim/lua/maik/remap.lua
@@ -7,6 +7,8 @@ vim.keymap.set("n", "J", "mzJ`z")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "{", "{zz")
+vim.keymap.set("n", "}", "}zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
