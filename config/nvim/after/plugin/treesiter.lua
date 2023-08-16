12fe3b1c9ded107bf8efc971d37f90c0435780bb
Michael Zoech <michi.zoech@gmail.com>
2023-08-15 21:32:38 -0700

    Add Erlang and Elixir to treesiter nvim plugin

--- config/nvim/after/plugin/treesiter.lua
+++ config/nvim/after/plugin/treesiter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "go", "java", "kotlin", "lua", "vim", "vimdoc", "query", "rust" },
+  ensure_installed = { "c", "elixir", "erlang", "go", "java", "kotlin", "lua", "vim", "vimdoc", "query", "rust" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
