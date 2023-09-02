1c6812d00f390e584409efaac9d7a9176401c579
Michael Zoech <michi.zoech@gmail.com>
2023-09-01 22:31:45 -0700

    C-p should fall back to regular file search if not in a Git repo

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,25 @@
 local builtin = require('telescope.builtin')
+
+local is_inside_work_tree = {}
+local git_or_find_files = function(opts)
+  local cwd = vim.fn.getcwd()
+  if is_inside_work_tree[cwd] == nil then
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    is_inside_work_tree[cwd] = vim.v.shell_error == 0
+  end
+  if is_inside_work_tree[cwd] then
+    builtin.git_files(opts)
+  else
+    builtin.find_files(opts)
+  end
+end
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+-- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', git_or_find_files, {})
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pr', builtin.resume, {})
+
