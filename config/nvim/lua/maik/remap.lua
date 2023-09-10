871f7c3d57d5f3d1c9b05153b4a84b6359abb20a
Michael Zoech <michi.zoech@gmail.com>
2023-09-09 18:09:03 -0700

    Disable moving based on display lines

--- config/nvim/lua/maik/remap.lua
+++ config/nvim/lua/maik/remap.lua
@@ -1,7 +1,7 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
+-- vim.keymap.set("n", "j", "gj")
+-- vim.keymap.set("n", "k", "gk")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
