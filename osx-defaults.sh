6f40d8d9b0d0718e1c3d205234970b0ae85de34c
Michael Zoech <michi.zoech@gmail.com>
2023-12-22 19:47:02 -0800

    Reworked OS X defaults settings that can be applied programmatically

---
+++ osx-defaults.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+
+# Show ~/Library folder
+chflags nohidden ~/Library
+
+# Change screenshot default from png to jpg
+defaults write com.apple.screencapture type jpg
+
+## For all input sources
+
+# No automatic capitalization
+defaults write -g NSAutomaticCapitalizationEnabled -bool false
+# No period with double-spaces
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
+# No automatic spell correction
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+## Dock
+
+# Dock on the left
+defaults write com.apple.dock orientation -string left
+# Faster minimize effect
+defaults write com.apple.dock mineffect -string scale
+# Instant dock toggle
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -int 0
+# Make hidden apps transparent in dock
+defaults write com.apple.dock showhidden -bool true
+
+killall Dock
+
