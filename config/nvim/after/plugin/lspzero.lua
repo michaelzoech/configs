07539935202a9d0fa4f9ed86e57d59d9951ae672
Michael Zoech <michi.zoech@gmail.com>
2024-02-12 14:15:09 +0100

    Alt+M for LSP code action

--- config/nvim/after/plugin/lspzero.lua
+++ config/nvim/after/plugin/lspzero.lua
@@ -15,6 +15,10 @@ lsp.on_attach(function(_client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp.default_keymaps({buffer = bufnr})
+
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("i", "<M-CR>", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "<M-CR>", vim.lsp.buf.code_action, bufopts)
 end)
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
